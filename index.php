f24230189e88ea36efed183c5361fa8216c76b37
Paladino75 <yagodearaujo75pc@gmail.com>
2021-07-02 20:56:47 -0400

    Adicionando validação de campos do formulário

--- index.php
+++ index.php
@@ -1,38 +1,22 @@
-<?php
-$categorias = [];
-$categorias[] = 'Infantil';
-$categorias[] = 'Adolescente';
-$categorias[] = 'Adulto';
+<!DOCTYPE html>
+<html>
 
-//print_r($categorias);
+<head>
+    <meta charser="utf-8">
+    <title>Formulário de inscrição</title>
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
 
-$nome = $_POST['nome'];
-$idade = $_POST['idade'];
+<body>
 
-//var_dump($nome);
-//var_dump($idade);
+<p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
 
-if($idade >= 6 && $idade <= 12 )
-{
-    for($i = 0; $i <= count($categorias); $i++)
-    {
-        if($categorias[$i] == 'infantil')
-            echo "O nadador ".$nome. " Compete na categoria " .$categorias[$i];
-    }
-}
-else if($idade >= 13 && $idade <=18)
-{
-    for($i = 0; $i <= count($categorias); $i++)
-    {
-        if($categorias[$i] == 'Adolescente')
-            echo "O nadador ".$nome. " compete na categoria". $categorias[$i];
-    }
-}
-else
-{
-    for($i = 0; $i <= count($categorias); $i++)
-    {
-        if($categorias[$i] == 'adulto')
-            echo "o nadador ".$nome. "compete na categoria adulto";
-    }
-}
+<form action="script.php" method="POST">
+    <p>Seu nome: <input type="text" name="nome"/></p>
+    <p>Sua idade: <input type="text" name="idade"/></p>
+    <p><input type="submit" value="Enviar dados do competidor"/></p>
+</form>
+</body>
+</html>
